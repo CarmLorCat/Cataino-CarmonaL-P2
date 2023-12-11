8bb7917e10d6e0a52878102a9f876ae722ff4c60
catarino carmona lorenzo <carmonc1@gmatc.matc.edu>
2023-12-10 21:57:48 -0600

    reinstall Client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,6 @@
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,9 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
