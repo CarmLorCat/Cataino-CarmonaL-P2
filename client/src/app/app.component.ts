ee18054686ca787ec1660dbe53619a4d9fc59ad6
catarino carmona lorenzo <carmonc1@gmatc.matc.edu>
2023-12-10 23:07:01 -0600

    done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  weatherForecasts: any;
+  constructor(private http:HttpClient){
+  
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.http.get('http://localhost:5207/weatherforecast').subscribe(
+      response=> { this.weatherForecasts = response;},
+      error => {console.log(error)}
+      );
+  }
 }
+
+
